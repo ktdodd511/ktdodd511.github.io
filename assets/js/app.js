434d45e7b33170a18864e79b334128ec00b4754d
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2017-02-21 16:37:22 -0800

    added projects, used jquery to have them fade in when clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,15 @@ console.log("app.js linked.");
 $(document).ready(function () {
 
 
+  $('#about-button').on('click', function () {
+    $('#about').fadeIn(4000);
+  });
 
+  $('#projects-button').on('click', function () {
+    $('#projects').fadeIn(4000);
+  });
+
+  $('#contact-button').on('click', function () {
+    $('#contact').fadeIn(4000);
+  });
 });
