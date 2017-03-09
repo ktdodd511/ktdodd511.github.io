1cf8ffb755206174061983000fa03755e7188bb3
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2017-03-09 15:57:41 -0800

    made individual pages for each project, just finished pupshare

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,64 +4,64 @@ $(document).ready(function () {
   $('#first-para').fadeIn(2000).removeClass('hidden');
   $('#second-para').fadeIn(10000).removeClass('hidden');
 
+
   $('#my-name').on('click', function() {
     window.location.replace('index.html')
   });
 
-  $(window).scroll(function() {
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn(400);
-		} else {
-			$('.scrollToTop').fadeOut(400);
-		}
-	});
 
-	//Click event to scroll to top
-	$('.scrollToTop').click(function() {
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
 
-  $('#pupshare').mouseover(function() {
-    $(this).animate({opacity: 0.5})
-    $(this).find('#pupshare-button').show();
+  //PUPSHARE//
+
+
+  $('#pupshare-img').on('click', function () {
+    window.location.replace('pupshare.html');
+
+  });
+
+  $('#pupshare-img').hover(function () {
+    $('.pupshare-view').show();
+
   },
     function () {
-      $(this).find('#pupshare-button').hide();
-  });
+      $('.pupshare-view').hide();
+});
 
-//PupShare buttons
-  $('#pupshare-button').click(function () {
-    window.location.replace('http://pupshare.herokuapp.com');
 
-  });
 
-  $('#pupshare-github-button').click(function () {
-    window.location.replace('https://github.com/ktdodd511/pupshare');
+//HIPTRAVELER//
 
-  });
 
-//Hiptraveler buttons
-  $('#hiptraveler-button').click(function () {
+  $('#hiptraveler-img').on('click', function () {
     window.location.replace('http://hiptraveler.herokuapp.com');
 
   });
 
-  $('#hiptraveler-github-button').click(function () {
-    window.location.replace('https://github.com/caseyjoneal/hip_traveler');
+  $('#hiptraveler-img').hover(function () {
+    $('.hip-view').show();
 
-  });
+  },
+    function () {
+      $('.hip-view').hide();
+});
 
-//ReviewThatBook buttons
-  $('#reviewthatbook-button').click(function () {
+
+
+//REVIEWTHATBOOK//
+
+
+  $('#book-img').click(function () {
     window.location.replace('http://reviewthatbook.herokuapp.com');
 
   });
 
-  $('#reviewthatbook-github-button').click(function () {
-    window.location.replace('https://github.com/ktdodd511/book_it');
+  $('#book-img').hover(function () {
+    $('.book-view').show();
 
-  });
+  },
+    function () {
+      $('.book-view').hide();
+});
 
 
 
