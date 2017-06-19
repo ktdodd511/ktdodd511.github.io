6db00175e9d990d0fd19a14d0b8aa0ccb9870127
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2017-06-19 11:02:16 -0700

    changed full stack developer words so that they type themselves across the screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,70 @@ $(document).ready(function () {
 
   });
 
+  // magically write words across the screen
+
+  var container = document.getElementById('homepage-sub-para');
+
+  var things = ['full stack developer'];
+  var t = -1;
+  var thing = '';
+  var message = container.innerHTML;
+  var mode = 'write';
+  var delay = 1000;
+
+  function updateText(txt) {
+      container.innerHTML = txt;
+  }
+
+  function tick() {
+
+      if(container.innerHTML.length == 0) {
+          t++;
+          thing = things[t];
+          message = '';
+          mode = 'write';
+      }
+
+      switch(mode) {
+          case 'write' :
+              message += thing.slice(0, 1);
+              thing = thing.substr(1);
+
+              updateText(message);
+
+              if(thing.length === 0 && t === (things.length - 1)) {
+                  window.clearTimeout(timeout);
+                  return;
+              }
+
+              if(thing.length == 0){
+                  mode = 'delete';
+                  delay = 1500;
+              } else {
+                  delay = 32 + Math.round(Math.random() * 40);
+              }
+
+              break;
+
+          case 'delete' :
+              message = message.slice(0, -1);
+              updateText(message);
+
+              if(message.length == 0)
+              {
+                  mode = 'write';
+                  delay = 1500;
+              } else {
+                  delay = 32 + Math.round(Math.random() * 100);
+              }
+              break;
+      }
+
+      timeout = window.setTimeout(tick, delay);
+  }
+
+  var timeout = window.setTimeout(tick, delay);
+
 
 
 
